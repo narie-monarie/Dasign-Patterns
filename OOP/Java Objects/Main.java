716f6f2b995a0879bf24dd543dd00ca98ab364f4
monarie <jobmonari7@gmail.com>
2023-03-31 19:02:05 +0300

    OOP

---
+++ OOP/Java Objects/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    // Objects are instances of a class that may contain attributes and methods
+    // Attributes - Characteristics
+    // Methods - Actions the object can perform
+  }
+}
