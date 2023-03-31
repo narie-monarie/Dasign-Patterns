75b5cb2d3ee70632168b2e7c123e8c716a9efdd4
monarie <jobmonari7@gmail.com>
2023-03-31 19:09:42 +0300

    Methods and Attributes

--- OOP/Java Objects/Main.java
+++ OOP/Java Objects/Main.java
@@ -3,5 +3,25 @@ public class Main {
     // Objects are instances of a class that may contain attributes and methods
     // Attributes - Characteristics
     // Methods - Actions the object can perform
+
+    Car myCar = new Car();
+    myCar.brake();
+  }
+}
+
+public class Car {
+  // Characteristics
+  String make = "Chevrolet";
+  String model = "Corvette";
+  int year = 2020;
+  String color = "blue";
+  double price = 50000.00;
+
+  void drive() {
+    System.out.println("You Drive the Car");
+  }
+
+  void brake() {
+    System.out.println("You Stepped on the Brake!");
   }
 }
