c44ac8d052eb1032f22615fc1d355d154227ebce
monarie <jobmonari7@gmail.com>
2023-04-02 20:00:34 +0300

    Factory pattern

--- Design Patterns/Factory/Main.java
+++ Design Patterns/Factory/Main.java
@@ -1,1 +1,75 @@
 
+public class Main {
+    public static void main(String[] args) {
+        BurgerFactory burgerFactory = new BurgerFactory();
+        System.out.println(burgerFactory.createCheeseBurger());
+        System.out.println(burgerFactory.createVeganBurger());
+        System.out.println(burgerFactory.createDeluxeBurger());
+    }
+
+}
+
+public class Burger {
+    private String ingredients;
+
+    public Burger(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+}
+
+public class BurgerFactory {
+    public String[] createCheeseBurger() {
+        String[] ingredients = { "bun", "cheese", "beef" };
+        return ingredients;
+    }
+
+    public String[] createDeluxeBurger() {
+        String[] ingredients = { "Tomato", "cheese", "beef" };
+        return ingredients;
+    }
+
+    public String[] createVeganBurger() {
+        String[] ingredients = { "bun", "sauce", "beef" };
+        return ingredients;
+    }
+
+}
+
+    
+
+    
+        
+
+
+
+    
+              
+    
+              
+    
+
+    
+
+        
+                
+        
+                
+        
+
+            
+            
+         
+
+                
+            
+                  
+        
+
+            
+            
+              
+        
+
+        
+            
