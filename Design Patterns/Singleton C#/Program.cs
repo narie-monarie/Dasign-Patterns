b1b2e908e3ae710ed047d2a3c4da5f231fc2768f
monarie <jobmonari7@gmail.com>
2023-04-04 12:29:08 +0300

    Singleton C#

---
+++ Design Patterns/Singleton C#/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SingleObject singleObject = SingleObject.getInstance();
+        singleObject.showMessage();
+    }
+}
