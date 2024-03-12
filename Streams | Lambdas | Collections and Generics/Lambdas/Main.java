2bceeb0b49a1b9e75dc5bc83d3db24643e33593e
Job Monari <jobmonari7@gmail.com>
2024-03-12 17:28:08 +0300

    streams

--- Streams | Lambdas | Collections and Generics/Lambdas/Main.java
+++ Streams | Lambdas | Collections and Generics/Lambdas/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class Main {
    public static void main(String[] args) {
@@ -6,5 +7,13 @@ public class Main {
     list.add("Hello ");
     list.add("World");
     list.forEach(System.out::print);
+
+    Employee[] arrayOfEmps = {
+    new Employee(1, "Jeff Bezos", 100000.0), 
+    new Employee(2, "Bill Gates", 200000.0), 
+    new Employee(3, "Mark Zuckerberg", 300000.0),
+    };
+
+    Stream.of(arrayOfEmps).forEach(e -> System.out.println(e.getName()));
    } 
 }
