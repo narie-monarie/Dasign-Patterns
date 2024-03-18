f05bea0f4cf4c7a365567327f46d51bbb69cbd56
Job Monari <jobmonari7@gmail.com>
2024-03-18 16:10:55 +0300

    update 01

--- Streams | Lambdas | Collections and Generics/Anonymous Classes/MainHello.java
+++ Streams | Lambdas | Collections and Generics/Anonymous Classes/MainHello.java
@@ -1,5 +1,11 @@
 //Anonymous Classes enable you to declare and instantiate a class at the same time
 public class Main {
+    /**
+     * InnerMain
+     */
+    interface Main {
+
+    }
 public static void main(String[] args) {
     
 }
---
+++ Streams | Lambdas | Collections and Generics/Lambdas/lambda 02/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        // ...
+    }
+
+    public void printPerson() {
+        // ...
+    }
+}
